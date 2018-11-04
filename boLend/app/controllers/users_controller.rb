8001fb9b0130271dcf2e2d3d1633f93f7a025975
Calvin Thai <calvinthai97@hotmail.com>
2018-11-03 21:15:56 -0400

    added new in controller

--- boLend/app/controllers/users_controller.rb
+++ boLend/app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+  def new 
+    @user = User.new
+  end
+
   def create
     @user = User.find(1) #should be the current login user
     @friend = User.find(params[:friend])
