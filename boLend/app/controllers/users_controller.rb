7df198d24ede47eac40fd86b57a3f87a68e5fe26
Calvin Thai <calvinthai97@hotmail.com>
2018-11-04 11:06:57 -0500

    merge error

--- boLend/app/controllers/users_controller.rb
+++ boLend/app/controllers/users_controller.rb
@@ -6,15 +6,26 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
-  def new 
-    @user = User.new
+  def new
   end
 
   def create
-    @user = User.find(1) #should be the current login user
-    @friend = User.find(params[:friend])
-    @user.friend_request(@friend)
-    render 'index'
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+
   end
+  private def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+
+  #def create
+   # @user = User.find(1) #should be the current login user
+    #@friend = User.find(params[:friend])
+  #  @user.friend_request(@friend)
+  #  render 'index'
+
+  #end
   
 end
