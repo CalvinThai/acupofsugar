91146555660916171446ec6625d6f24e5f419b97
Calvin Thai <calvinthai97@hotmail.com>
2018-11-04 11:05:21 -0500

    implementing sessions (login)

--- boLend/app/controllers/sessions_controller.rb
+++ boLend/app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
 
 	end
 	def create
-		render plain: params[:session].inspect
+		user = User.find_by(name: params[:name])
+		if user and user.authenticate(params[:password])
+			sesions[:user_id] = user.id
+			redirect_to admin_url
+		else
+			redirect_to login_url
+		end
+
 	end
 
 	def destroy
--- boLend/config/routes.rb
+++ boLend/config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
+  get 'admin/index'
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/index'
+  get 'users/new'
 
   resources :users do
   	resources :items
   	resources :on_hold_items
   	resources :wish_lists
+  end
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
   end
 
 end
