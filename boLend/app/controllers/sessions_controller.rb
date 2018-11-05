17bc89fbba4c0735e317c3fbc308c276a9bc2f29
Calvin Thai <calvinthai97@hotmail.com>
2018-11-05 17:15:52 -0500

    ww
    Signed-off-by: Calvin Thai <calvinthai97@hotmail.com>

--- boLend/app/controllers/sessions_controller.rb
+++ boLend/app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password_digest])
-      sessions[:uuser_id] = user.id
-      redirect_to admin_url
+    #user = User.find_by_email(params[:email])
+    #user = User.find_by_email(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
+   # if user && user.authenticate(params[:password])
+   #if user && user.authenticate(params[:password_digest])
+   if user && user.authenticate(params[:session][:password_digest])
+      flash[:notice] = "suceess"
+      session[:user_id] = user.id
+      redirect_to login_url
       # Log the user in and redirect to the user's show page.
     else
+      flash[:notice] = "rekt"
       redirect_to login_url
     end
   end
