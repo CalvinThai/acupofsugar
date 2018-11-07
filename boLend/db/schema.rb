349708eed1c933c29019b0b19d4be77563066b37
Ramya Prasad <Ramya.Prasad@mail.utoronto.ca>
2018-11-07 16:58:49 -0500

    Add Conversation and Message model and controller

--- boLend/db/schema.rb
+++ boLend/db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_171531) do
 
+ActiveRecord::Schema.define(version: 2018_11_07_171531) do
   create_table "borrowed_items", force: :cascade do |t|
     t.integer "user_id"
     t.integer "item_id"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_11_07_171531) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_borrowed_items_on_item_id"
     t.index ["user_id"], name: "index_borrowed_items_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "user1_id"
+    t.integer "user2_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -41,6 +48,17 @@ ActiveRecord::Schema.define(version: 2018_11_07_171531) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "on_hold_items", force: :cascade do |t|
