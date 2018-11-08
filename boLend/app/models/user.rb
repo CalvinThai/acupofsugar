1b1c5db4ca2f0956d1e93cacca46b060793aea71
Calvin Thai <calvinthai97@hotmail.com>
2018-11-08 17:27:15 -0500

    password reset works now (through email, cba to make recovery questions..)

--- boLend/app/models/user.rb
+++ boLend/app/models/user.rb
@@ -27,6 +27,19 @@ class User < ApplicationRecord
     save!(:validate => false)
   end
 
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def generate_token(column)
+    begin
+      self[column]= SecureRandom.urlsafe_base64
+    end while User.exists?(column=> self[column])
+  end
+
 
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -34,7 +47,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+ # validates :password, presence: false, length: { minimum: 6 }
 
   private
   def confirmation_token
