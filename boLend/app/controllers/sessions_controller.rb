a5764b1a9b967164f0e5c02471924d58f00d6fbc
Calvin Thai <calvinthai97@hotmail.com>
2018-11-17 14:41:13 -0500

    already logged in message

--- boLend/app/controllers/sessions_controller.rb
+++ boLend/app/controllers/sessions_controller.rb
@@ -4,27 +4,32 @@ class SessionsController < ApplicationController
   	
   end
   def create
-  if request.env['omniauth.auth']
-    user = User.create_with_omniauth(request.env['omniauth.auth'])
-    session[:user_id] = user.id    
-    redirect_to user_path(user.id)
+  if (session[:user_id] != nil)
+    flash[:failure] = "Already logged in"
+    redirect_to login_url
   else
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password_digest])
-      if user.email_confirmed
-        flash[:failure] = "suceess"
-        session[:user_id] = user.id
-        redirect_to user #should login to the user page
-        # Log the user in and redirect to the user's show page.
-        
-        #redirect_to profile_path #save for later use, redirect user to /profile page rather than /user/:id - by jiehao
-      else 
-        flash[:failure] =  'Please verify your email address.'
+    if request.env['omniauth.auth']
+      user = User.create_with_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = user.id    
+      redirect_to user_path(user.id)
+    else
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password_digest])
+        if user.email_confirmed
+          flash[:failure] = "suceess"
+          session[:user_id] = user.id
+          redirect_to user #should login to the user page
+          # Log the user in and redirect to the user's show page.
+          
+          #redirect_to profile_path #save for later use, redirect user to /profile page rather than /user/:id - by jiehao
+        else 
+          flash[:failure] =  'Please verify your email address.'
+          redirect_to login_url
+        end
+      else
+        flash[:failure] = "rekt"
         redirect_to login_url
       end
-    else
-      flash[:failure] = "rekt"
-      redirect_to login_url
     end
   end
 end
