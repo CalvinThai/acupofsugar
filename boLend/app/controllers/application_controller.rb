2fdb89ec54f366081459575e6a2fa2c6c92a2acd
jiehao <jiehao.liu@mail.utoronto.ca>
2018-11-29 12:16:45 -0500

    now you have to login to access available services

--- boLend/app/controllers/application_controller.rb
+++ boLend/app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-
+before_action :require_login
   def current_user
   	#change after adding session
   	User.find_by(id: session[:user_id])
     #User.find(2)
   end
     helper_method :current_user
+    
+  private
+    def require_login
+      unless current_user
+        redirect_to login_url
+      end
+    end
 end
