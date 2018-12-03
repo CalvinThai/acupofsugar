81455ec7987471c36c6d4c33f7192724c0da00e9
Calvin Thai <calvinthai97@hotmail.com>
2018-12-03 14:12:05 -0500

    123

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,12 +54,7 @@ class User < ApplicationRecord
 
   def self.find_or_create_from_auth_hash(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      if User.find_by(auth.info.email.downcase)?
-        user = User.find_by(auth,info.email.downcase)
-        user.email_confirmed = true
-        user.provider = auth.provider
-        user.uid = auth.uid
-      else
+
         user.provider = auth.provider
         user.uid = auth.uid
         user.fname = auth.info.first_name
